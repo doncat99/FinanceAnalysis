02e89f2a0214eaff16964fd5cd25a2f9249b2ada
Don Wong <doncat99@gmail.com>
2020-09-21 15:13:48 +0800

    add stats support

--- run.py
+++ run.py
@@ -3,12 +3,14 @@ warnings.filterwarnings("ignore")
 
 import logging
 
+import pandas as pd
 from plotly.subplots import make_subplots
 import plotly.graph_objects as go
 
 from zvt import zvt_env
 from zvt.contract.common import Region, Provider
 from zvt.factors.squeeze_factor import SqueezeFactor
+import zvt.stats as qs
 
 logger = logging.getLogger(__name__)
 
@@ -35,11 +37,16 @@ def chart(dfs):
             fig.show()   
 
 if __name__ == '__main__':
-    factor = SqueezeFactor(region=Region.US, entity_ids=['stock_NYSE_A', 'stock_NYSE_DDD'], 
+    factor = SqueezeFactor(region=Region.US, entity_ids=['stock_NASDAQ_FB'], 
                            start_timestamp='2015-01-01', end_timestamp='2020-07-01',
                            kdata_overlap=4)
 
     gb = factor.result_df.groupby('entity_id')
     dfs = [gb.get_group(x) for x in gb.groups]
-    chart(dfs)
+
+    target_df = dfs[0]
+
+    stock = pd.Series(target_df.close.pct_change().tolist(), index=target_df.timestamp)
+    qs.reports.html(returns=stock, benchmark="SPY", output='a.html')
+    # chart(dfs)
 
